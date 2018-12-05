217f0e0ed80dc473887223eb2c30143bf608d7f7
Sumover <test@runnoob.com>
2018-12-06 00:38:16 +0800

    delete a 文件夹

--- src/EducationManagement.java
+++ src/EducationManagement.java
@@ -1,7 +1,7 @@
 import java.sql.*;
-        import java.util.Iterator;
-        import java.util.LinkedList;
-        import java.util.List;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class EducationManagement {
