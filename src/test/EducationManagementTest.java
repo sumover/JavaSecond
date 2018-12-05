f32c8d35aafdeb47b1972aff7764cf6a942c18f7
Sumover <test@runnoob.com>
2018-12-06 07:04:19 +0800

    Junit

---
+++ src/test/EducationManagementTest.java
@@ -0,0 +1,74 @@
+package test;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * EducationManagement Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>Ê®¶þÔÂ 6, 2018</pre>
+ */
+public class EducationManagementTest {
+
+    @Before
+    public void before() throws Exception {
+
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: getStudentByNo(String sno)
+     */
+    @Test
+    public void testGetStudentByNo() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: setPassword(String number, String password)
+     */
+    @Test
+    public void testSetPassword() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: addStudents(List<Student> students)
+     */
+    @Test
+    public void testAddStudents() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: deleteStudent(String number)
+     */
+    @Test
+    public void testDeleteStudent() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: getStudentByCity(String City)
+     */
+    @Test
+    public void testGetStudentByCity() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: close()
+     */
+    @Test
+    public void testClose() throws Exception {
+//TODO: Test goes here...
+    }
+
+
+} 
